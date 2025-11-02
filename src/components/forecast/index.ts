88e9a2b6053c4e81957847d826d77dd44bf62d15
Obeid99 <onouicer@gmail.com>
2025-11-02 19:49:04 +0100

    feat: add new business dashboard components to forecast index

--- src/components/forecast/index.ts
+++ src/components/forecast/index.ts
@@ -2,6 +2,15 @@
 export { default as ForecastArticleCard } from "./ForecastArticleCard";
 export { default as ForecastDetailPanel } from "./ForecastDetailPanel";
 export { default as ForecastMetricsDashboard } from "./ForecastMetricsDashboard";
+export { default as HistoricalProgressChart } from "./HistoricalProgressChart";
+export { default as ModelPerformanceCard } from "./ModelPerformanceCard";
+export { default as ModelComparison } from "./ModelComparison";
 export { ForecastTable } from "../ForecastTable";
 export { ForecastSettings } from "../ForecastSettings";
 export { KPICard } from "../KPICard";
+
+// New business dashboard components
+export { default as BusinessMetricsOverview } from "./BusinessMetricsOverview";
+export { default as TopProductsRanking } from "./TopProductsRanking";
+export { default as CategoryPerformance } from "./CategoryPerformance";
+export { default as ProductCatalogExplorer } from "./ProductCatalogExplorer";
